a822ef7b66059c4632de9c49654b7b07826eadc5
erenmentes <emreakgunasd1@gmail.com>
2025-10-14 23:04:25 +0300

    idkk

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,11 @@
+<<<<<<< HEAD
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+=======
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth.guard';
+>>>>>>> 6098974 (idk)
 
 @Controller('auth')
 export class AuthController {
@@ -10,4 +16,13 @@ export class AuthController {
     signIn(@Body() signInDto : Record<string,any>) {
         return this.authService.signIn(signInDto.username, signInDto.password);
     };
+<<<<<<< HEAD
+=======
+
+    @UseGuards(AuthGuard)
+    @Get('profile')
+    getProfile(@Request() req) {
+        return req.user;
+    };
+>>>>>>> 6098974 (idk)
 };
